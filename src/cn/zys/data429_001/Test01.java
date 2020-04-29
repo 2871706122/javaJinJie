6abd52ed4eafd7422ac0717cf39a1ddccf4e2b33
zhouyusen <GFf2g23t>
2020-04-29 22:38:35 +0800

    在家测试拉取代码进行编辑和环境的测试

--- src/cn/zys/data429_001/Test01.java
+++ src/cn/zys/data429_001/Test01.java
@@ -1,6 +1,47 @@
 package cn.zys.data429_001;
-
+//构建一个标准类
+//1.成员变量用private修饰
+//2.为成员变量设置一组getter和setter
+//3.设置一个无参数的构造函数
+//4.设置一个全参数的构造函数
 public class Test01 {
+    private String name;
+    private char sex;
+    private int age;
+
+    public Test01() {
+    }
+
+    public Test01(String name, char sex, int age) {
+        this.name = name;
+        this.sex = sex;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public char getSex() {
+        return sex;
+    }
+
+    public void setSex(char sex) {
+        this.sex = sex;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
     public static void main(String[] args) {
         System.out.println("yes");
     }
