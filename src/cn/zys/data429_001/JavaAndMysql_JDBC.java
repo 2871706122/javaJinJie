2ea3464b77c86ba2ea4c8673e137e09641d7f5a5
zhouyusen <zhouyusen>
2020-05-09 20:41:54 +0800

    1

--- src/cn/zys/data429_001/JavaAndMysql_JDBC.java
+++ src/cn/zys/data429_001/JavaAndMysql_JDBC.java
@@ -7,9 +7,10 @@ import java.sql.Statement;
 public class JavaAndMysql_JDBC {
     public static void main(String[] args) throws Exception {
         Class.forName("com.mysql.jdbc.Driver");
-        Connection conObj = DriverManager.getConnection("jdbc:mysql://localhost:3306/day430","root","8411");
+        //需要注意一下自己电脑上的端口是3306，数据库名称和表的名称注意要修改才能使用
+        Connection conObj = DriverManager.getConnection("jdbc:mysql://localhost:8411/zys001","root","8411");
 
-        String sql = "update usersname set name = 100 where id = 3";
+        String sql = "update user_msg set sex = '男' where id = 2";
 
         Statement stm = conObj.createStatement();
 
