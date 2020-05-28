cb06260ed0269500cdffd62bf54f420be6f5d55a
zhouyusen <zhouyusen>
2020-05-28 20:59:31 +0800

    spring template javabean结果类型返回

--- src/cn/zys/day520/doDolder/JdbcTemplate_demo2_select.java
+++ src/cn/zys/day520/doDolder/JdbcTemplate_demo2_select.java
@@ -1,9 +1,13 @@
 package cn.zys.day520.doDolder;
 
+import cn.zys.day520.tablesObj.User_msg_data;
 import cn.zys.day520.uitles.Jdbc_druid_uitles;
 import org.junit.Test;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Map;
 
@@ -36,5 +40,37 @@ public class JdbcTemplate_demo2_select {
         //{id=1, name=张三, age=20, sex=男}
         //{id=2, name=王五, age=10, sex=男}
         //{id=14, name=name, age=20, sex=女}
+    }
+
+    /**
+     * 将结果封装成emp对象的list集合(java bean对象
+     */
+    @Test
+    public void getDataBean(){
+        String sql = "select * from user_msg";
+        List<User_msg_data> arr = jdbcTemplateObj.query(sql, new RowMapper<User_msg_data>() {
+            @Override
+            public User_msg_data mapRow(ResultSet resultSetObj, int i) throws SQLException {
+                User_msg_data user_msg_dataObj = new User_msg_data();
+                int id = resultSetObj.getInt("id");
+                String name = resultSetObj.getString("name");
+                int age = resultSetObj.getInt("age");
+                String sex = resultSetObj.getString("sex");
+                user_msg_dataObj.setId(id);
+                user_msg_dataObj.setName(name);
+                user_msg_dataObj.setAge(age);
+                user_msg_dataObj.setSex(sex);
+
+                return user_msg_dataObj;
+            }
+        });
+
+        for (User_msg_data obj : arr) {
+            System.out.println(obj);
+        }
+        //结果:
+        //User_msg_data{id=1, name='张三', age=20, sex='男'}
+        //User_msg_data{id=2, name='王五', age=10, sex='男'}
+        //User_msg_data{id=14, name='name', age=20, sex='女'}
     }
 }
